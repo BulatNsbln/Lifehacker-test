c4861403de49d2d22a4f6776ed90a0d280f0e403
Bulat Nasibullin <nasibullinbulat95@gmail.com>
2019-04-04 19:06:40 +0400

    Added react-router-dom

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
+import { Link, Route } from 'react-router-dom';
+
+import Article from './Article';
 
 const apiEndpoint = 'https://lifehacker.ru/api/wp/v2/posts';
 
@@ -19,20 +22,36 @@ const apiEndpoint = 'https://lifehacker.ru/api/wp/v2/posts';
            });
     };
 
+     getArticle = ({ match }) => {
+         console.log('---', 'article match: ', match)
+         return <Article id={match.params.id} isOpen key={match.params.id} />
+     }
+
     render() {
         let { posts } = this.state;
         if(posts.length === 0) return <h1> Loading </h1>
         return(
-            <ul>
-                { posts.map( article => {
-                    return (
-                        <Fragment>
-                            <img src={article.cat_cover.sizes.mobile} alt='лого'></img>
-                            <li> { article.title.rendered } </li>
-                        </Fragment>
-                    )
-                })}
-            </ul>
+            <Fragment>
+                <Link to="/">Home</Link>
+                <ul>
+                    { posts.map( article => {
+                        console.log(article.id, 22222)
+                        let id = article.id;
+                        return (
+                            <Fragment>
+                                <img src={article.cat_cover.sizes.mobile} alt='лого'></img>
+                                <li>
+                                    <Link to={`/articles:${id}`}>
+                                        { article.title.rendered }
+                                    </Link>
+                                </li>
+                            </Fragment>
+                        )
+                    })}
+                </ul>
+                <hr />
+                <Route path="/articles:id" render={this.getArticle} />
+            </Fragment>
         )
     };
 }
