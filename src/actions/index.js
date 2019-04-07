2f0c85fcf83a5ab9866a2f644915d6fa6701753e
Bulat Nasibullin <nasibullinbulat95@gmail.com>
2019-04-07 23:18:01 +0400

    Deleted proxy

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,9 @@
 import { LOAD_ALL_ARTICLES } from '../constants';
 
-// Для CORS
-const proxy = 'https://cors-anywhere.herokuapp.com/';
 
 export function loadAllArticles() {
     return {
         type: LOAD_ALL_ARTICLES,
-        callAPI: proxy + 'https://lifehacker.ru/api/wp/v2/posts'
+        callAPI: 'https://lifehacker.ru/api/wp/v2/posts'
     }
 };
