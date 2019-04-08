1be4122a713dd0bec78675842e708a00a842efd6
Bulat Nasibullin <nasibullinbulat95@gmail.com>
2019-04-08 06:59:38 +0400

    Updates

---
+++ precache-manifest.60a47f528f15a3479ea3a724ef4a53b7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bcf4ba045c8a53d7684",
+    "url": "/BulatNsbln/Lifehacker-test/static/js/main.d7e92bb4.chunk.js"
+  },
+  {
+    "revision": "d9db5ba6665fb5e7d128",
+    "url": "/BulatNsbln/Lifehacker-test/static/js/runtime~main.771544e7.js"
+  },
+  {
+    "revision": "7c811b19b3014ae6aec8",
+    "url": "/BulatNsbln/Lifehacker-test/static/js/2.d3775382.chunk.js"
+  },
+  {
+    "revision": "aad93aa6aeccec5ff941c3eb3f6bd5ca",
+    "url": "/BulatNsbln/Lifehacker-test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0549598677d847bfe6ab45554b077ad0.js"
+  "/BulatNsbln/Lifehacker-test/precache-manifest.60a47f528f15a3479ea3a724ef4a53b7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/BulatNsbln/Lifehacker-test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
